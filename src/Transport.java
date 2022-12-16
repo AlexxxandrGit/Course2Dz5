44080631c751268421c934686e6cdd7e07fa0a7a
Alexandr <kitalex777@mail.ru>
2022-12-16 07:25:14 +0300

    my commit 1. DZ8

--- src/Transport.java
+++ src/Transport.java
@@ -1,16 +1,14 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public abstract class Transport {
     private String brand;
     private String model;
     private double engineVolume;
 
-    private final List<Driver<?>> drivers = new ArrayList<>();
-    private final List<Mechanic<?>> mechanics = new ArrayList<>();
+    private final Set<Driver<?>> drivers = new HashSet<>();
+    private final Set <Mechanic<?>> mechanics = new HashSet<>();
 
-    private final List<Sponsor> sponsors = new ArrayList<>();
+    private final Set <Sponsor> sponsors = new HashSet<>();
 
 
 
@@ -120,15 +118,15 @@ public abstract class Transport {
         sponsors.add(sponsor);
     }
 
-    public List<Driver<?>> getDrivers() {
+    public Set <Driver<?>> getDrivers() {
         return drivers;
     }
 
-    public List<Mechanic<?>> getMechanics() {
+    public Set <Mechanic<?>> getMechanics() {
         return mechanics;
     }
 
-    public List<Sponsor> getSponsors() {
+    public Set <Sponsor> getSponsors() {
         return sponsors;
     }
 
